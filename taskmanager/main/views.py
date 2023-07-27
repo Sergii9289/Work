e1771ab35aca3cce0e7d6eca0d9e03260845ad05
Sergii9289 <zx9289@gmail.com>
2023-07-27 21:07:26 +0300

    Правильно прописал сортировку БД в admin.py

--- taskmanager/main/views.py
+++ taskmanager/main/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render
 from .models import Task
 
 def index(request):
-    tasks = Task.objects.all() #выводит все объекты в виде списка
-    # tasks = Task.objects.order_by(id)  # выводит сортированный список
-    return render(request, 'main/index.html', {'title': 'Головна сторінка сайту', 'tasks': tasks})
+    # tasks = Task.objects.all() # выводит все объекты в виде списка
+    tasks = Task.objects.order_by('-id')[:2] # выводит сортированный список (если поставить '-', то сортировка в обратном порядке).
+    # [:4] срез списка. Сколько значений будет выведено из БД
+    return render(request, 'main/index.html', {'title': 'Головна сторінка сайту', 'tasks': tasks}) # в словаре в конце передаёи информацию для jinja
     # через jinja передаёт значения 'title' и 'tasks' HTML документ
 
 def about(request):
