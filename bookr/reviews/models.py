c9b33c83d709d22f2ec31ac8a0dbc071248893af
Sergii9289 <zx9289@gmail.com>
2024-01-16 21:12:05 +0200

    many-to-many added. Class BookContributor

--- bookr/reviews/models.py
+++ bookr/reviews/models.py
@@ -21,6 +21,7 @@ class Book(models.Model):
         max_length=20,
         verbose_name='ISBN number of the book.')
     publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
+    contributors = models.ManyToManyField('Contributor', through='BookContributor')
 
 
 class Contributor(models.Model):
@@ -33,3 +34,15 @@ class Contributor(models.Model):
     email = models.EmailField(
         help_text='Contact email for the contributor.')
 
+
+class BookContributor(models.Model):
+    class ContributionRole(models.TextChoices):
+        AUTHOR = 'AUTHOR', 'Author'
+        CO_AUTHOR = 'CO_AUTHOR', 'Co-Author'
+        EDITOR = 'EDITOR', 'Editor'
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    contributor = models.ForeignKey(Contributor, on_delete=models.CASCADE)
+    role = models.CharField(
+        verbose_name='The role this contributor had in book',
+        choices=ContributionRole.choices,
+        max_length=20)
